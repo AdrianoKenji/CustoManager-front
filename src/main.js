f24c79262f0345ce1910650de110fef88185b2d6
Adriano Kenji <adriano0608.kenji@gmail.com>
2022-05-05 05:25:27 -0300

    Bug fix no router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router.js'
+import router from './router'
 import VMask from "vue-the-mask"
 
 import 'bootstrap/dist/js/bootstrap.min.js'
